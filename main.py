5abc0187b605cfec7ac380bd3c988008e09e80cd
Joshua Long <131176908+Sadliquid@users.noreply.github.com>
2024-05-07 18:53:47 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+import requests, os
+from dotenv import load_dotenv
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+load_dotenv()
+
+API_KEY = os.environ.get('API_KEY')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/searchPlayerTag', methods=['POST'])
+def search():
+    if "playerTag" not in request.json:
+        return "One or more required payloads missing"
+    
+    PLAYER_TAG = request.json['playerTag']
+
+    url = f'https://api.brawlstars.com/v1/players/%23{PLAYER_TAG}'
+
+    headers = {
+        'Authorization': f'Bearer {API_KEY}'
+    }
+
+    response = requests.get(url, headers=headers)
+
+    print(response.json())
+
+    if response.status_code == 200:
+        player_info = response.json()
+        return jsonify(player_info)
+    else:
+        return "Player not found"
+
+if __name__ == '__main__':
+    app.run(debug=True)
